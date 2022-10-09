fcff97a099cab036cef3c494ecea74cb015f5235
Leandro Moreira <leandro@leandromoreirati.com.br>
2022-10-09 11:23:58 -0300

    fix: OWASP Ignore issue.

--- zap.sh
+++ zap.sh
@@ -8,11 +8,11 @@ echo $(id -u):$(id -g)
 #docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
 
 # Generate ConfigFile
-if [[ -z gen_file ]]; then
-   docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -g gen_file
-else
-   echo "File existe"
-fi
+#if [[ -z gen_file ]]; then
+#   docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -g gen_file
+#else
+#   echo "File existe"
+#fi
 
 # comment above cmd and uncomment below lines to run with CUSTOM RULES
 docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap-rules -w report.md -J json_report.json -r zap_report.html
